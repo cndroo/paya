7ecb21491096a8a3ba289a7cb1d6f0ed3076628c
cndroo <131943795+cndroo@users.noreply.github.com>
2024-03-08 21:18:13 +0700

    tesan.py

--- tesan.py
+++ tesan.py
@@ -21,7 +21,7 @@ rr = random.randint
 rc = random.choice
 #<----------[ USER AGENT ]---------->#
 ugen=[]
-for snighdho in range(5)
+for snighdho in range(5):
 try:
 	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=80000&country=all&ssl=all&anonymity=all').text
 	open('.prox.txt','w').write(prox)
